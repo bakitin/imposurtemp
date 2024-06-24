add1402bf7d1640b551bfbe69e27671158dab06f
bakitin <137194209+bakitin@users.noreply.github.com>
2024-06-24 14:17:07 -0500

    Delete Inicio directory

--- Inicio/script.js
+++
@@ -1,6 +0,0 @@
-window.addEventListener('load', function() {
-    setTimeout(function() {
-        var loader = document.getElementById('loader');
-        loader.style.display = 'none';
-    }, 4000); // Oculta el GIF después de 4 segundos
-});
