8e63c2426759f5cd4913cd79460838051be74771
bakitin <137194209+bakitin@users.noreply.github.com>
2024-06-24 14:17:33 -0500

    Add files via upload

---
+++ Inicio/script.js
@@ -0,0 +1,6 @@
+window.addEventListener('load', function() {
+    setTimeout(function() {
+        var loader = document.getElementById('loader');
+        loader.style.display = 'none';
+    }, 4000); // Oculta el GIF después de 4 segundos
+});
